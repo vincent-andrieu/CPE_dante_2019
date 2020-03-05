9aef0614621f2943b8eac9207a141a69a54de586
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-03-05 15:11:26 +0100

    Add dante.h

---
+++ include/dante.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** Dante's star
+** File description:
+** header dante
+*/
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef DANTE_H_
+#define DANTE_H_
+
+#define EXIT_ERROR 84
+#define WALL 'X'
+#define EMPTY '*'
+
+#endif
