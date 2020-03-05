450f4e61a3b76bc522f86bbc4c7da20aa57953f9
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-03-05 22:01:41 +0100

    Add lib (my_strlen & my_str_to_array)

---
+++ include/my.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** my lib .h
+** File description:
+** all prototypes
+*/
+
+#include <stdbool.h>
+
+#ifndef MY_H
+#define MY_H
+
+#define READ_SIZE 10
+
+int my_strlen(char const *str);
+char **my_str_to_array(char const *str, char const *c_list, bool kp_splts);
+
+#endif
